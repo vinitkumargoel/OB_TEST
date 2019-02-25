395683ae37108e63a3f0c2e92ec83a1b71cd092a
M Sathish - (MAS) <MS396584@wipro.com>
2019-02-25 16:32:36 +0530

    getInstructions api added

--- twoWayPoolService/routes/index.js
+++ twoWayPoolService/routes/index.js
@@ -7,9 +7,8 @@ var axios = require('axios');
 var request = require('request');
 var fs = require('fs');
 
-
-
 router.post('/populateInstruction', function (req, res) {
+
   let token = req.headers['x-access-token'];
   let controlBankDetails = req.body.controlBank;
   let contraBankDetails = req.body.contraBank;
@@ -26,7 +25,7 @@ router.post('/populateInstruction', function (req, res) {
       .then((resp) => {
         Object.assign(data, resp.data);
         let resObj = {
-          instructionID: parseInt(data['instruction-list'].length) + 1001,
+          instructionID: parseInt(data['instruction-list'].length) + 1000,
           priorityID: parseInt(data['instruction-list'].length) + 1,
           controlBank: controlBankDetails.bankName,
           controlBankAccountNumber: controlBankDetails.value,
@@ -38,7 +37,7 @@ router.post('/populateInstruction', function (req, res) {
         }
         let finalData = data['instruction-list'];
         finalData.push(resObj);
-        request.patch({
+        request.post({
           url: serviceUrlConfig.dbUrl+'/'+userName+'-instructions',
           body: {
             'instruction-list': finalData
@@ -51,34 +50,6 @@ router.post('/populateInstruction', function (req, res) {
       });
   });
 });
-
-
-
-
-router.get('/getInstruction',(req,res)=> {
-  var token = req.headers['x-access-token'];
-
-    jwt.verify(token, config.secret, function (err, decodedObj) {        
-        if (err) {
-            return res.status(401).send({
-                auth: false,
-                message: 'Failed to authenticate token.'
-            });
-        };
-        let userName = decodedObj.username;
-
-        request
-            .get(`${serviceUrlConfig.dbUrl}/${userName}-instructions`, (err, response, body) => {
-                if(err) {
-                    res.status(500).json({
-                        errorMsg: 'User data not available'
-                    });
-                }
-                //let instructionSet = JSON.parse(body);
-                res.status(200).json(body);
-            });
-    });
-})
 
 router.get('/transaction', function (req, res, next) {
   let token = req.headers['x-access-token'];
@@ -166,7 +137,7 @@ const instrResult=async(userName)=>{
         if(controlBankBalance == target){
           result.push({
             "priority": priorityID,
-            "message": "Control Bank balance is same as target",
+            "message": "Target Balance is same as in the account",
             "controlBankBalance": controlBankBalance,
             "contraBankBalance": contraBankBalance
           });
@@ -189,7 +160,15 @@ const instrResult=async(userName)=>{
             
         }
         else if(controlBankBalance < target) {
-           if ((contraBankBalance - target - contraBankBalance) > contraBankMinBalance) {
+          if((target - controlBankBalance)>contraBankBalance){
+            result.push({
+              "priority": priorityID,
+             "message": "Insufficient funds in the account",
+             "controlBankBalance": controlBankBalance,
+             "contraBankBalance": contraBankBalance
+           });
+          }
+           else if ((contraBankBalance - target - contraBankBalance) >= contraBankMinBalance) {
              contraBankBalance -= target - contraBankBalance;
              controlBankBalance = target;
               //console.log(contraBankBalance,controlBankBalance);
@@ -205,7 +184,9 @@ const instrResult=async(userName)=>{
             }else {
              result.push({
                "priority": priorityID,
-              "message": "Transaction failed,ContraBank minimun balanace cannot be achieved"
+              "message": "Transaction failed,ContraBank minimun balanace cannot be achieved",
+              "controlBankBalance": controlBankBalance,
+              "contraBankBalance": contraBankBalance
             });
           
         }
@@ -213,7 +194,8 @@ const instrResult=async(userName)=>{
 
         
      };
-
+    //  console.log(result);
+    //  console.log(typeof result);
      return result;
    };
    
@@ -242,7 +224,7 @@ let updateTransaction=async(userName,bank,priorityID,controlBankBalance,contraBa
   try{
   let resp=await axios.patch(serviceUrlConfig.dbUrl + '/' + userName + '-commercial',{'banks': bank})
   if(resp){
-   // console.log(response);
+    console.log(resp.data);
     
   };
   newObj={
@@ -259,4 +241,214 @@ let updateTransaction=async(userName,bank,priorityID,controlBankBalance,contraBa
 };
 
 
+router.get('/instructions',(req,res)=>{
+  let token = req.headers['x-access-token'];
+  
+
+  jwt.verify(token, config.secret, function (err, decodedObj) {
+     if (err) return res.status(500).json({
+       auth: false,
+     message: 'Failed to authenticate token.'
+     });
+
+     let userName = decodedObj.username;
+   // console.log(userName);
+    let result= getInstruction(userName);
+  
+    
+    result.then(function(data) {
+     // console.log(data) ;
+      res.send(data);
+   })
+   .catch((err)=>{
+    console.log(err);
+   })
+
+    });
+});
+
+
+router.get('/balances',(req,res)=>{
+  let token = req.headers['x-access-token'];
+  
+
+  jwt.verify(token, config.secret, function (err, decodedObj) {
+     if (err) return res.status(500).json({
+       auth: false,
+     message: 'Failed to authenticate token.'
+     });
+
+     let userName = decodedObj.username;
+   // console.log(userName);
+     getInstruction(userName).then(function(instrObj) {
+       
+        // console.log(instrObj);
+      
+           let len = instrObj["instruction-list"].length;
+           let result = [];
+        let i=0;
+           
+          let controlBank, contraBank, controlBankAccountNumber, contraBankAccountNumber, target, controlBankBalance, contraBankBalance, contraBankMinBalance;
+      
+          //to get last value of ID	
+          //for (i = 0; i < len; i++) {
+            controlBank = instrObj["instruction-list"][i].controlBank;
+            contraBank = instrObj["instruction-list"][i].contraBank;
+            controlBankAccountNumber= instrObj["instruction-list"][i].controlBankAccountNumber;
+            contraBankAccountNumber= instrObj["instruction-list"][i].contraBankAccountNumber;
+            target = parseInt(instrObj["instruction-list"][i].target);
+            priorityID = parseInt(instrObj["instruction-list"][i].priorityID);
+            //console.log(target);
+      
+            getcommercialAcct(userName).then(function(data) {
+                 // console.log(data) ;
+                 var filteredControlBank = data.banks.filter((bank) => {
+                  return bank.bankName == controlBank;
+               })[0];
+       
+               //console.log(filteredControlBank);
+       
+               var filteredContraBank = data.banks.filter((bank) => {
+                  return bank.bankName == contraBank;
+               })[0];
+               var restBankDetails = data.banks.filter((bank) => {
+                  return bank.bankName != contraBank && bank.bankName != controlBank;
+               });
+       
+               var filteredControlAcc=filteredControlBank["accounts"].filter((acc)=>{
+                 return acc.accountNumber == controlBankAccountNumber;
+               });
+       
+               var filteredContraAcc=filteredContraBank["accounts"].filter((acc)=>{
+                 return acc.accountNumber == contraBankAccountNumber;
+               });
+       
+               
+               // console.log(filteredControlBank);
+               //console.log(filteredControlAcc);
+               // console.log(filteredContraAcc[0].balance);
+               
+               controlBankBalance = filteredControlAcc[0].balance;
+               contraBankBalance = filteredContraAcc[0].balance;
+               contraBankMinBalance = filteredContraAcc[0].minBalance;   
+     
+               res.send({controlBankBalance,contraBankBalance,contraBankMinBalance});
+          
+
+               })
+               .catch((err)=>{
+                console.log(err);
+               });      
+           
+              //console.log(data);
+             
+      //   result.then(function(data) {
+      //    // console.log(data) ;
+      //     res.send(data);
+      //  })
+      //  .catch((err)=>{
+      //   console.log(err);
+      //  })
+    
+        //};   
+          // return(result);
+       })
+       .catch((err)=>{
+        console.log(err);
+      });
+   });
+});
+
+
 module.exports = router;
+
+
+// request.get(serviceUrlConfig.dbUrl+'/'+userName+'-instructions', function (err, response, body) {
+//   if (err) return res.status(500).json({
+//     message: 'Failed to load data'
+//   })
+//   // console.log(body, postData.transfers);
+//   var instrObj = JSON.parse(body);
+//   var len = Object.keys(instrObj).length;
+//   if(len===0) {
+//     res.send("Empty for now");
+//   }
+
+//       for (i = 0; i < len; i++) {
+//         if (instrObj[userName].instructions[i].instrId > max)
+//           max = instrObj[userName].instructions[i].instrId;
+
+//         if (instrObj[userName].instructions[i].priorityId > prior)
+//           prior = instrObj[userName].instructions[i].instrId;
+//       }
+
+//       var result={
+//         instrId: max + 1,
+//         controlBank,
+//         cntrlBnkAccId,
+//         contraBank,
+//         cntraBnkAccId,
+//         target,
+//         priorityId: prior + 1
+//       };
+
+
+//       request.patch({
+//         url: serviceUrlConfig.dbUrl + '/' + userName + '-commercial',
+//         body: {
+//           'banks': data.banks
+//         },
+//         json: true
+//       }, function (err, response, body) {
+//         if (err) return res.status(500).json({
+//           message: 'Failed to patch data'
+//         })
+//         console.log(body);
+//         result.push({
+//           "priority": priorityId,
+//           "message": "transaction successful"
+//         });
+//         //res.status(200).json(body);
+//       });
+
+//     }  
+// /*
+//     let instrObj = require('../data/instructions.json');
+//     if(Object.keys(instrObj).length===0) {
+//       res.send("Empty for now");
+//     }
+
+//     let len = instrObj[userName].instructions.length;
+//     let max = 0,
+//       prior = 0;
+
+//     //to get last value of ID	
+//     for (i = 0; i < len; i++) {
+//       if (instrObj[userName].instructions[i].instrId > max)
+//         max = instrObj[userName].instructions[i].instrId;
+
+//       if (instrObj[userName].instructions[i].priorityId > prior)
+//         prior = instrObj[userName].instructions[i].instrId;
+//     }
+
+//     //pushing the request body data to the array
+//     instrObj[userName].instructions.push({
+//       instrId: max + 1,
+//       controlBank,
+//       contraBank,
+//       target,
+//       priorityId: prior + 1
+//     });
+
+//     instrObj[userName].instructions.sort((a, b) => (a.priorityId > b.priorityId) ? 1 : ((b.priorityId > a.priorityId) ? -1 : 0))
+//     //writing the file synchronously
+//     try {
+//       fs.writeFileSync(__dirname + "/../data/instructions.json", JSON.stringify(instrObj, null, 2) );
+//     } catch (err) {
+//       if (err) {
+//         throw err;
+//       }
+//     }
+//     //console.log(obj);
+//     res.send(instrObj[userName]);
+//   */
